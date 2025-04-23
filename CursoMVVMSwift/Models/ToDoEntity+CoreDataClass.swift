3acaac4c66f1a0b0c5fa4aff99a9fbcd90cd337e
Paul Flores <paulfloresdev@icloud.com>
2025-04-22 21:47:36 -0600

    First commit

---
+++ CursoMVVMSwift/Models/ToDoEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoEntity+CoreDataClass.swift
+//  CursoMVVMSwift
+//
+//  Created by Paul Flores on 22/04/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class ToDoEntity: NSManagedObject {
+
+}
