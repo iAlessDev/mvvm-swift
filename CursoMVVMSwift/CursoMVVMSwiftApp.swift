f68407a541a3ddfad9d146978a33721c184216d4
Paul Flores <paulfloresdev@icloud.com>
2025-04-25 20:28:03 -0600

    Main entry app point configuration

--- CursoMVVMSwift/CursoMVVMSwiftApp.swift
+++ CursoMVVMSwift/CursoMVVMSwiftApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct CursoMVVMSwiftApp: App {
+    // The main view model for the app
+    // StateObject is used to create and manage the lifecycle of the view model
+    // This class will be instantiated once and shared across the app
+    @StateObject private var viewModel = ToDoViewModel()
+    
+    // The main entry point of the app
     var body: some Scene {
         WindowGroup {
+            // Calls the ContentView and passes the view model
+            // like an environment object
             ContentView()
+                .environmentObject(viewModel)
         }
     }
 }
