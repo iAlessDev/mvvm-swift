7c8b534b95cbe6c54dfba37a5a18f7df05327b1e
Paul Flores <paulfloresdev@icloud.com>
2025-04-25 20:25:59 -0600

    Create persistance data logic

--- CursoMVVMSwift/Commons/ToDoPersistenceManager.swift
+++ CursoMVVMSwift/Commons/ToDoPersistenceManager.swift
@@ -7,13 +7,14 @@
 
 import CoreData
 
+// this class is used to manage the persistence of the data
 final class ToDoPersistenceManager {
     static let shared = ToDoPersistenceManager()
     
     let container: NSPersistentContainer
     
     private init() {
-        container = NSPersistentContainer(name: "DataModel")
+        container = NSPersistentContainer(name: "ModelData")
         container.loadPersistentStores{ storeDescription, error in
             if let error = error {
                 print("Error cargando datos: \(error)")
