b3f09f366339019c0ca7da9063e81268706121dc
Paul Flores <paulfloresdev@icloud.com>
2025-04-25 20:32:26 -0600

    Create base view structure for archived toDos

---
+++ CursoMVVMSwift/Views/ToDoArchived/ToDoArchivedListView.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoArchivedListView.swift
+//  CursoMVVMSwift
+//
+//  Created by Paul Flores on 24/04/25.
+//
+
+import SwiftUI
+
+public struct ToDoArchivedListView: View {
+    
+    
+    public var body: some View {
+        ScrollView {
+            
+        }
+        .padding(.top, 20)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("Archivadas")
+    }
+    
+}
+
+#Preview {
+    ToDoArchivedListView()
+        
+}
