c8c59aaa45a122c6e518d42c15e5eb9a4df9400b
Paul Flores <paulfloresdev@icloud.com>
2025-04-29 16:03:02 -0600

    Create no todo view logic

--- CursoMVVMSwift/Views/ToDoList/NoToDosView.swift
+++ CursoMVVMSwift/Views/ToDoList/NoToDosView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct NoToDosView: View {
+    @State public var typeOfView: TypeOfView
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Image(systemName: typeOfView.imageOfView)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .foregroundColor(.gray.opacity(0.7))
+            
+            Text(typeOfView.textOfView)
+                .font(.title2.bold())
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.top, 80)
+    }
+}
+
+enum TypeOfView {
+    case task
+    case archived
+
+    var imageOfView: String {
+        switch self {
+        case .task:
+            return "text.page.slash.rtl"
+        case .archived:
+            return "tray"
+        }
+    }
+    
+    var textOfView: String {
+        switch self {
+        case .task:
+            return "There are no tasks"
+        case .archived:
+            return "There are no archived tasks"
+        }
     }
 }
 
 #Preview {
-    NoToDosView()
+    NoToDosView(typeOfView: .task)
 }
