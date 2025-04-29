ff1a6e955f3647fcb02188a01638c4fb2f695c88
Paul Flores <paulfloresdev@icloud.com>
2025-04-28 21:01:03 -0600

    Change date to an optional value

--- CursoMVVMSwift/Models/Item.swift
+++ CursoMVVMSwift/Models/Item.swift
@@ -12,7 +12,7 @@ public struct Item: Identifiable, Equatable {
     public let id: String = UUID().uuidString
     public var title: String
     public var note: String?
-    public var date: Date
+    public var date: Date?
     public var isCompleted: Bool
     public var isArchived: Bool
 }
