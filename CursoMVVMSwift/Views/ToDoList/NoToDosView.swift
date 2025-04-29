24efbbd5d6390d46bbdc25a82de46df70b5fd378
Paul Flores <paulfloresdev@icloud.com>
2025-04-29 16:01:48 -0600

    Create view where don't have toDos

---
+++ CursoMVVMSwift/Views/ToDoList/NoToDosView.swift
@@ -0,0 +1,18 @@
+//
+//  NoToDosView.swift
+//  CursoMVVMSwift
+//
+//  Created by Paul Flores on 29/04/25.
+//
+
+import SwiftUI
+
+struct NoToDosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoToDosView()
+}
