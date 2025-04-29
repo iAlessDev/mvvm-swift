02207bb18f7d549b27c4de85ff232fc97165d6d0
Paul Flores <paulfloresdev@icloud.com>
2025-04-28 21:05:36 -0600

    Create toDo Status file and his logic

--- CursoMVVMSwift/Views/ToDoList/ToDoStatusView.swift
+++ CursoMVVMSwift/Views/ToDoList/ToDoStatusView.swift
@@ -9,28 +9,31 @@
 import SwiftUI
 
 public struct ToDoStatusView: View {
-    
+    @EnvironmentObject private var viewModel: ToDoViewModel
+    public let toDo: ToDoEntity
     
     public let disabled: Bool
     
     public init(
-        
-        
+        toDo: ToDoEntity,
         disabled: Bool = false
     ) {
-        
-        
+        self.toDo = toDo
         self.disabled = disabled
     }
     
     public var body: some View {
-       
+        let imageName: String = toDo.isCompleted ? "checkmark.circle.fill" : "circle"
+        let width: CGFloat = toDo.isCompleted ? 24 : 18
+        let height: CGFloat = width
         
-        return Image("checkmark.circle.fill")
+        
+        return Image(systemName: imageName)
+            .resizable()
+            .frame(width: width, height: height)
+            .foregroundStyle(Color.white)
+            
     }
     
 }
 
-#Preview {
-   
-}
